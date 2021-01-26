e6462e7103682a48b7e0831fa08b9edacdd6b626
Evan Pascual <evanpascual@evans-imac.home>
2021-01-26 11:32:07 -0800

    set up

---
+++ app.js
@@ -0,0 +1,33 @@
+const intro = document.querySelector('.intro');
+const video = intro.querySelector('video');
+const text = intro.querySelector('h1');
+//END SECTION
+const section = document.querySelector('section');
+const end = section.querySelector('h1');
+
+//SCROLL MAGIC
+const controller = new ScrollMagic.Controller();
+
+//SCENES
+const scene = new ScrollMagic.Scene({
+    duration: 6000,
+    triggerElement: intro, 
+    triggerHook: 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+//VIDEO ANIMATION
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+    scrollpos = e.scrollPos / 1000;
+})
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    video.currentTime = delay;
+}, 30);
